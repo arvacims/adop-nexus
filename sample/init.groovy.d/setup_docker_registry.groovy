284e2547d1e65aecd8cbaf8e2a910480af3d8802
hein168 <vitali.heinrich@arvato.com>
2018-09-05 15:00:53 +0200

    repo setup for docker and npm

--- sample/init.groovy.d/setup_docker_registry.groovy
+++ sample/init.groovy.d/setup_docker_registry.groovy
@@ -1,3 +1,25 @@
 import org.sonatype.nexus.repository.storage.WritePolicy
 
-repository.createDockerHosted("docker-private", 5000, null, "default", false, true, WritePolicy.ALLOW_ONCE)
+// https://github.com/sonatype/nexus-public/blob/master/plugins/nexus-script-plugin/src/main/java/org/sonatype/nexus/script/plugin/internal/provisioning/RepositoryApiImpl.groovy
+// https://github.com/sonatype/nexus-public/blob/master/plugins/nexus-script-plugin/src/main/java/org/sonatype/nexus/script/plugin/RepositoryApi.java
+
+privateRepo = "docker-private"
+println "Try to get repo: $privateRepo"
+if (repository.repositoryManager.get(privateRepo) == null) {
+    println "Repo not found. Add repo: $privateRepo"
+    repository.createDockerHosted(privateRepo, 5000, null, "default", false, true, WritePolicy.ALLOW_ONCE)
+}
+
+proxyRepo = "docker-hub"
+println "Try to get repo: $proxyRepo"
+if (repository.repositoryManager.get(proxyRepo) == null) {
+    println "Repo not found. Add repo: $proxyRepo"
+    repository.createDockerProxy(proxyRepo, "https://registry-1.docker.io", "HUB", null, null, null)
+}
+
+repoGroup = "docker-all"
+println "Try to get repo: $repoGroup"
+if (repository.repositoryManager.get(repoGroup) == null) {
+    println "Repo not found. Add repo: $repoGroup"
+    repository.createDockerGroup(repoGroup, null, null, [proxyRepo, privateRepo])
+}
--- sample/init.groovy.d/setup_npm_registry.groovy
+++ sample/init.groovy.d/setup_npm_registry.groovy
@@ -1,7 +1,25 @@
 import org.sonatype.nexus.repository.storage.WritePolicy
 
-repoName = "npm-private"
+// https://github.com/sonatype/nexus-public/blob/master/plugins/nexus-script-plugin/src/main/java/org/sonatype/nexus/script/plugin/internal/provisioning/RepositoryApiImpl.groovy
+// https://github.com/sonatype/nexus-public/blob/master/plugins/nexus-script-plugin/src/main/java/org/sonatype/nexus/script/plugin/RepositoryApi.java
 
-repository.createNpmHosted(repoName, "default", true, WritePolicy.ALLOW)
-repository.createNpmProxy("npmjs-org", "https://registry.npmjs.org", "default")
-repository.createNpmGroup("npm-all", ["npmjs-org", repoName], "default")
+privateRepo = "npm-private"
+println "Try to get repo: $privateRepo"
+if (repository.repositoryManager.get(privateRepo) == null) {
+    println "Add repo: $privateRepo"
+    repository.createNpmHosted(privateRepo, "default", true, WritePolicy.ALLOW)
+}
+
+proxyRepo = "npmjs-org"
+println "Try to get repo: $proxyRepo"
+if (repository.repositoryManager.get(proxyRepo) == null) {
+    println "Add repo: $proxyRepo"
+    repository.createNpmProxy(proxyRepo, "https://registry.npmjs.org", "default")
+}
+
+repoGroup = "npm-all"
+println "Try to get repo-group: $repoGroup"
+if (repository.repositoryManager.get(repoGroup) == null) {
+    println "Add repo: $repoGroup"
+    repository.createNpmGroup(repoGroup, [proxyRepo, privateRepo], "default")
+}
